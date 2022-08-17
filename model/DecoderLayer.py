408e12249887b9599936ed3a1e746aecdfea9fec
Jungmin-YUN-0 <76892989+Jungmin-YUN-0@users.noreply.github.com>
2022-08-17 17:12:40 +0900

    Update DecoderLayer.py

--- model/DecoderLayer.py
+++ model/DecoderLayer.py
@@ -2,21 +2,48 @@ import torch.nn as nn
 from model.MultiHeadAttention import MultiHeadAttention
 from model.FeedForward import FeedForward
 
-
 # decoder layer
 
 ##masked_, cross_, feedforward_
 class DecoderLayer(nn.Module):
-    def __init__(self, d_model, n_head, ffn_hidden, drop_prob, device):
+    def __init__(self, d_model, n_head, ffn_hidden, drop_prob, device, attn_option, n_position):
         super().__init__()
 
+        self.attn_option = attn_option
         self.self_attention = MultiHeadAttention(d_model, n_head, drop_prob, device)
+        self.self_attn_layer_norm = nn.LayerNorm(d_model)
         self.enc_dec_attention = MultiHeadAttention(d_model, n_head, drop_prob, device)
+        self.enc_dec_layer_norm = nn.LayerNorm(d_model)
+
         self.ffn = FeedForward(d_model=d_model, hidden=ffn_hidden, drop_prob=drop_prob)
+        self.ffn_layer_norm = nn.LayerNorm(d_model)
         
-    def forward(self, trg, enc_src, src_mask=None, trg_mask=None):
-        out, dec_slf_attn = self.self_attention(trg, trg, trg, mask=src_mask)
-        out, dec_enc_attn = self.enc_dec_attention(out, enc_src, enc_src, mask=trg_mask)
-        out = self.ffn(out)
-        # residual connection, layer normalization은 각각의 sublayer에서 미리 수행하였음
-        return out, dec_slf_attn, dec_enc_attn
+        self.dropout1 = nn.Dropout(drop_prob)
+        self.dropout2 = nn.Dropout(drop_prob)
+        self.dropout3 = nn.Dropout(drop_prob) #!#
+
+    def forward(self, trg, enc_src, trg_mask, src_mask):
+        # 1-1# masked multi-head attention      
+        _trg, _ = self.self_attention(trg, trg, trg, trg_mask)
+
+        # 1-2# dropout, residual connection, layer normalization
+        trg = trg + self.self_attn_layer_norm(self.dropout1(_trg))
+        #x = self.self_attn_layer_norm(trg + self.dropout1(_trg)) #!#
+        
+        # 2-1# cross multi-head attention (encoder-decoder attention)
+        _trg, attention = self.enc_dec_attention(trg, enc_src, enc_src, src_mask)
+
+        # 2-2# dropout, residual connection, layer normalization
+        trg = trg + self.enc_dec_layer_norm(self.dropout2(_trg))
+        #x = self.enc_dec_layer_norm(x + self.dropout2(_trg))
+        
+        # 3-1# feed-forward neural network
+        _trg = self.ffn(trg)
+        # 3-2# dropout, residual connection, layer normalization
+        trg = trg + self.ffn_layer_norm(self.dropout3(_trg))
+        #x = self.ffn_layer_norm(x + self.dropout3(_trg)) #!#
+
+        return trg, attention
+
+
+        #src = src + self.attn_layer_norm(self.dropout1(_src)) #!#

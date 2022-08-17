d1338936cce3cfab3d397f37bd74c48e4bd95e64
Jungmin-YUN-0 <76892989+Jungmin-YUN-0@users.noreply.github.com>
2022-08-17 17:14:13 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,120 +1,196 @@
-import spacy
 import torch
-from torchtext.data.metrics import bleu_score  # BLEU
 import dill
+from tqdm import tqdm
 from torchtext.data import Dataset, BucketIterator
-import hparams
+from torchtext.data.metrics import bleu_score  # BLEU
 from model.Transformer import Transformer
+from model.Classification import CF_Transformer
+from model.translator import Translator
+import argparse
+import wandb
+import torch.nn.functional as F
+#from sklearn.metrics import classification_report
+from sklearn.metrics import f1_score
+#from sklearn.metrics import precision_score
+#from sklearn.metrics import recall_score
+from sklearn import metrics
 
-#############
-# inference #
-#############
+class Test():
+    def __init__(self, gpu, opt, batch_size, data_pkl, model_save, pred_save, hidden_dim, n_layer, n_head, ff_dim, dropout, data_task):
 
-## translation
-def translate_sentence(sentence, src_field, trg_field, model, device, max_len=50, logging=True):
-    model.eval() # 평가 모드
+        self.data_pkl = data_pkl
+        self.saved_model = model_save
+        self.saved_result = pred_save
 
-    # tokenization
-    if isinstance(sentence, str):
-        nlp = spacy.load('de')
-        tokens = [token.text.lower() for token in nlp(sentence)]
-    else:
-        tokens = [token.lower() for token in sentence]
+        gpu = "cuda:"+gpu
+        device = torch.device(gpu if torch.cuda.is_available() else 'cpu')  # cuda(gpu) 사용
 
-    # start with <sos> token, end with <eos> token
-    tokens = [src_field.init_token] + tokens + [src_field.eos_token]
+        self.HIDDEN_DIM = hidden_dim
+        self.ENC_LAYERS = self.DEC_LAYERS = n_layer
+        self.ENC_HEADS = self.DEC_HEADS = n_head
+        self.ENC_PF_DIM = self.DEC_PF_DIM = ff_dim
+        self.ENC_DROPOUT = self.DEC_DROPOUT = dropout
+        attn_option = opt
+   
+        ##########################################################################################################
+        data = dill.load(open(self.data_pkl, 'rb'))
 
-    src_indexes = [src_field.vocab.stoi[token] for token in tokens]
-    src_tensor = torch.LongTensor(src_indexes).unsqueeze(0).to(device)
+        SRC, TRG = data['vocab']['src'], data['vocab']['trg']
+        SRC_PAD_IDX = SRC.vocab.stoi["<blank>"]
+        if data_task == "MT":
+            TRG_PAD_IDX = TRG.vocab.stoi["<blank>"]
+            TRG_SOS_IDX = TRG.vocab.stoi["<sos>"]
+            TRG_EOS_IDX = TRG.vocab.stoi["<eos>"]
 
-    # source -> mask 생성
-    src_mask = model.make_src_mask(src_tensor)
+        INPUT_DIM = len(SRC.vocab)
+        OUTPUT_DIM = len(TRG.vocab)
 
-    # encoder(input sentence) -> output
-    with torch.no_grad():  # gradient 계산 x (inference니까 필요x, memory,, 연산속도,,)
-        enc_src = model.encoder(src_tensor, src_mask)
+        test_data = Dataset(examples=data['test_data'], fields={'src': SRC, 'trg': TRG})
+        test_iterator = BucketIterator(test_data, batch_size=batch_size, device=device)
 
-    ## start with a <sos> token
-    trg_indexes = [trg_field.vocab.stoi[trg_field.init_token]]
+        if data_task == "MT":
+            model = Transformer(INPUT_DIM, OUTPUT_DIM, SRC_PAD_IDX, TRG_PAD_IDX, self.HIDDEN_DIM,
+                            self.ENC_LAYERS, self.DEC_LAYERS, self.ENC_HEADS, self.DEC_HEADS,
+                            self.ENC_PF_DIM, self.DEC_PF_DIM, self.ENC_DROPOUT, self.DEC_DROPOUT, device, attn_option).to(device)
+        elif data_task == "CF":
+            model = CF_Transformer(INPUT_DIM, OUTPUT_DIM, SRC_PAD_IDX, self.HIDDEN_DIM, self.ENC_LAYERS, self.ENC_HEADS, self.ENC_PF_DIM, self.ENC_DROPOUT, device, attn_option).to(device)
 
-    for i in range(max_len):
-        trg_tensor = torch.LongTensor(trg_indexes).unsqueeze(0).to(device)
+        model.load_state_dict(torch.load(self.saved_model))
+        print('[Info] Trained model state loaded.')
 
-        # target -> mask 생성
-        trg_mask = model.make_trg_mask(trg_tensor)
+        
 
-        with torch.no_grad():
-            output, attention = model.decoder(trg_tensor, enc_src, trg_mask, src_mask)
+        #wandb.init(project="transformer", entity="jungminy")
 
-        # 출력 문장에서 가장 마지막 단어만 사용
-        pred_token = output.argmax(2)[:,-1].item()
-        trg_indexes.append(pred_token) # 출력 문장에 더하기
+        #import torchvision.models as models
+        #from ptflops import get_model_complexity_info
 
-        # end with <eos> token
-        if pred_token == trg_field.vocab.stoi[trg_field.eos_token]:
-            break
+        #dummy_size = (256, 1)
 
-    # 각 출력 단어의 인덱스 -> 실제 단어로 변환
-    trg_tokens = [trg_field.vocab.itos[i] for i in trg_indexes]
+        #macs, params = get_model_complexity_info(model, dummy_size, as_strings=True, print_per_layer_stat=False, verbose=False)
+                                        
+        #print('computational complexity: ', macs)
+        #print('number of parameters: ', params)
 
-    # 첫 번째 <sos>는 제외 후, 출력 문장 반환
-    return trg_tokens[1:], attention
+        #===============================================================#
+        if data_task == 'MT':
+            translator = Translator(model=model, beam_size=5, max_seq_len=512, src_pad_idx=SRC_PAD_IDX, trg_pad_idx=TRG_PAD_IDX, trg_bos_idx=TRG_SOS_IDX, trg_eos_idx=TRG_EOS_IDX, device=device).to(device)
+            unk_idx = SRC.vocab.stoi[SRC.unk_token]
 
-########################
-# calculate bleu_score #
-########################
-def show_bleu(data, src_field, trg_field, model, device, max_len=50):
-    trgs = []
-    pred_trgs = []
-    index = 0
+            pred_trgs = []
+            trgs= []
+            index=0
+            
+            print('[Info] Inference ...')
+            if attn_option == 'BASE':
+                with open(self.saved_result, 'w') as f:
+                    for example in tqdm(test_data, desc='  - (Test)', leave=False):
+                        #print(' '.join(example.src))
+                        src_seq = [SRC.vocab.stoi.get(word, unk_idx) for word in example.src]
 
-    for datum in data:
-        src = vars(datum)['src']
-        trg = vars(datum)['trg']
+                        src_seq_ext = torch.zeros((1,512), dtype=torch.long) #!#
+                        src_seq_ext[:, :len(src_seq)] = torch.LongTensor([src_seq]) #!#
+                        pred_seq = translator.translate_sentence(src_seq_ext.to(device)) #!#
 
-        pred_trg, _ = translate_sentence(src, src_field, trg_field, model, device, max_len, logging=False)
+                        pred_seq = translator.translate_sentence(torch.LongTensor([src_seq]).to(device))
+                        
+                        pred_line = ' '.join(TRG.vocab.itos[idx] for idx in pred_seq)
+                        #pred_line = pred_line.replace("<sos>", '').replace("<eos>", '')
+                        pred_line = pred_line.replace("<sos>", '').replace("<unk>", '').replace("<eos>", '')
+                        #print(pred_line)
+                        f.write(pred_line.strip() + '\n')
+                        #f.write(' '.join(vars(example)['trg']) + '\n')
 
-        # <eos> token 제거
-        pred_trg = pred_trg[:-1]
+                        ## for BLEU_score
+                        pred_trgs.append(pred_line.split(" ")) ##예측            
+                        trgs.append([vars(example)['trg']]) ##정답
+                        if (index%100)==0:
+                            print(f"[{index} / {len(test_data)}")
+                            print(f'예측: {pred_line}')
+                            print(f"정답: {' '.join(vars(example)['trg'])}")
+                        index += 1
+                bleu = bleu_score(pred_trgs, trgs, max_n=4, weights=[0.25,0.25,0.25,0.25])
+                print('[Info] Finished.')
+                print(f'BLEU score : {bleu}')
+            
+            elif attn_option == 'LR' or attn_option =='CT':
+                with open(self.saved_result, 'w') as f:
+                    for example in tqdm(test_data, desc='  - (Test)', leave=False):
+                        src_seq = [SRC.vocab.stoi.get(word, unk_idx) for word in example.src]
+                        
+                        src_seq_ext = torch.zeros((1,512), dtype=torch.long) #!#
+                        src_seq_ext[:, :len(src_seq)] = torch.LongTensor([src_seq]) #!#
+                        pred_seq = translator.translate_sentence(src_seq_ext.to(device)) #!#
 
-        pred_trgs.append(pred_trg)
-        trgs.append([trg])
-        index += 1
-        if (index + 1) % 100 == 0:
-            print(f"[{index + 1}/{len(data)}]")
-            print(f"예측: {pred_trg}")
-            print(f"정답: {trg}")
+                        pred_line = ' '.join(TRG.vocab.itos[idx] for idx in pred_seq)
+                        pred_line = pred_line.replace("<sos>", '').replace("<unk>", '').replace("<eos>", '')
+                        #print(pred_line)
+                        f.write(pred_line.strip() + '\n')
 
-    bleu = bleu_score(pred_trgs, trgs, max_n=4, weights=[0.25, 0.25, 0.25, 0.25])
-    return bleu
+                        ## for BLEU_score
+                        pred_trgs.append(pred_line.split(" ")) ##예측            
+                        trgs.append([vars(example)['trg']]) ##정답
+                        if (index%100)==0:
+                            print(f"[{index} / {len(test_data)}")
+                            print(f'예측: {pred_line}')
+                            print(f"정답: {' '.join(vars(example)['trg'])}")
+                        index += 1
+                        
+                bleu = bleu_score(pred_trgs, trgs, max_n=4, weights=[0.25,0.25,0.25,0.25])
+                print('[Info] Finished.')
+                print(f'BLEU score : {bleu}')
+            #wandb.log({"bleu_score": bleu})
 
-##############################################################################
+            return
+##################################################################################################################
+        if data_task == 'CF':
+            def get_predictions(model, iterator, device):
+                model.eval()
 
-device = torch.device('cuda:2' if torch.cuda.is_available() else 'cpu')  # cuda(gpu) 사용
-batch_size = 64
+                epoch_accuracy = 0
+                
+                targets = torch.tensor([]).to(device)
+                preds= torch.tensor([]).to(device)
 
-data = dill.load(open("data.pickle", 'rb'))
+                with torch.no_grad():
+                    for batch in tqdm(iterator):
+                        input_seq, _ = batch.src
+                        
+                        target = batch.trg
+                        pred = model(input_seq)
 
-SRC_PAD_IDX = data['vocab']['src'].vocab.stoi["<blank>"]
-TRG_PAD_IDX = data['vocab']['trg'].vocab.stoi["<blank>"]
+                        pred_class = pred.argmax(dim=-1)
+                        correct_pred = pred_class.eq(target).sum()
+                        accuracy = correct_pred / pred.shape[0]
 
-INPUT_DIM = len(data['vocab']['src'].vocab)  # src_vocab_size
-OUTPUT_DIM = len(data['vocab']['trg'].vocab)  # trg_vocab_size
+                        epoch_accuracy += accuracy.item()         
 
-fields = {'src': data['vocab']['src'], 'trg' : data['vocab']['trg']}
+                        targets = torch.cat([targets,target])
+                        preds = torch.cat([preds, pred_class])
 
-#train_data = Dataset(examples=data['train_data'], fields=fields)
-#val_data = Dataset(examples=data['valid_data'], fields=fields)
-test_data = Dataset(examples=data['test_data'], fields=fields)
+                        #F1_score = f1_score(target.cpu(), pred_class.cpu(), average='binary')
+                        #Recall_score =  recall_score(target.cpu(), pred_class.cpu(), average='binary')
+                        #Precision_score = precision_score(target.cpu(), pred_class.cpu(), average='binary')
+                        #F1_score = f1_score(target.cpu(), pred_class.cpu(), average='weighted')#!!#average=binary
+                        #F1_score_t = tfa.metrics.F1Score(num_classes=2, average="weighted")
+                        
+                        #print(metrics.classification_report(target.cpu(), pred_class.cpu(), digits=4))
+                    #F1_score = f1_score(targets.cpu(), preds.cpu(), average='binary')# number of class = 2
+                    F1_score = f1_score(targets.cpu(), preds.cpu(), average='weighted') # number of class > 2
+                return epoch_accuracy / len(iterator), F1_score
 
-test_iterator = BucketIterator(test_data, batch_size=batch_size, device=device)
+            acc, F1_score = get_predictions(model, test_iterator, device)
 
-## Create Model
-model = Transformer(INPUT_DIM, OUTPUT_DIM, SRC_PAD_IDX, TRG_PAD_IDX,hparams.HIDDEN_DIM,
-                                hparams.ENC_LAYERS, hparams.DEC_LAYERS, hparams.ENC_HEADS, hparams.DEC_HEADS,
-                                hparams.ENC_PF_DIM, hparams.DEC_PF_DIM, hparams.ENC_DROPOUT, hparams.DEC_DROPOUT, device).to(device)
+            print(f"accuracy: {acc}")
+            print(f"f1_score: {F1_score}")
+            #print(f"f1_score(tensorflow): {F1_score_t}")
 
+            #print(f"recall_score: {Recall_score}")
+            #print(f"precision_score: {Precision_score}")
+        # from thop import profile
+        # flops, params = profile(model, inputs=(torch.randn(1,64).to(device).long(),), verbose=False)
+        # print(flops, params)
+##################################################################################################################
 
-
-bleu = show_bleu(test_data, data['vocab']['src'], data['vocab']['trg'], model, device)
-print(bleu)
+if __name__ == "__main__":
+    Test()

bc489ef91a9b21da0a227ac205541e8b094ca022
Jungmin-YUN-0 <76892989+Jungmin-YUN-0@users.noreply.github.com>
2022-08-17 17:12:13 +0900

    Update Encoder.py

--- model/Encoder.py
+++ model/Encoder.py
@@ -1,3 +1,4 @@
+from doctest import OutputChecker
 import torch.nn as nn
 import torch
 from model.EncoderLayer import EncoderLayer
@@ -9,32 +10,41 @@ import copy
 
 ##embedding + encoder layer
 class Encoder(nn.Module):
-    def __init__(self, INPUT_DIM, SRC_PAD_IDX, HIDDEN_DIM, ENC_LAYERS, ENC_HEADS, ENC_PF_DIM, ENC_DROPOUT, device):
+    def __init__(self, enc_voc_size, SRC_PAD_IDX, d_model, n_layers, n_head, ffn_hidden, drop_prob, device, attn_option, n_position=512):
         super().__init__()
-
         self.device = device
-        self.tok_embedding = nn.Embedding(INPUT_DIM, HIDDEN_DIM, padding_idx=SRC_PAD_IDX)
-        self.pos_embedding = PositionalEncoding(HIDDEN_DIM, n_position=512)
+        self.attn_option = attn_option
 
-        encoder_layer = EncoderLayer(HIDDEN_DIM, ENC_HEADS, ENC_PF_DIM, ENC_DROPOUT, device)
-        self.layers = nn.ModuleList([copy.deepcopy(encoder_layer) for _ in range(ENC_LAYERS)])
-
-        self.dropout = nn.Dropout(ENC_DROPOUT)
-        self.HIDDEN_DIM = HIDDEN_DIM # (=d_model)
-        self.layer_norm = nn.LayerNorm(HIDDEN_DIM)
-
-        self.d_model = HIDDEN_DIM
-#d_model, n_head, ffn_hidden, drop_prob, device):
-    def forward(self, src_seq, src_mask):
+        # input dimenstion → embedding dimension
+        self.tok_embedding = nn.Embedding(enc_voc_size, d_model, padding_idx=SRC_PAD_IDX)
+        # positional embedding 학습 (sinusoidal x) =>> self.pos_embedding = nn.Embedding(max_len, d_model)
+        self.pos_encoding = PositionalEncoding(d_model, n_position=n_position)
+        #self.layers = nn.ModuleList([EncoderLayer.EncoderLayer(d_model, n_head, ffn_hidden, drop_prob, device) for _ in range(n_layers)])
+        encoder_layer = EncoderLayer(d_model, n_head, ffn_hidden, drop_prob, device, attn_option, n_position)
+        self.layers = nn.ModuleList([copy.deepcopy(encoder_layer) for _ in range(n_layers)])
+        self.dropout = nn.Dropout(drop_prob)
+        #self.d_model = d_model
+        self.scale = torch.sqrt(torch.FloatTensor([d_model])).to(device)  ## normalization (가중치에 d_model을 곱함)
         
-        tok_emb = self.tok_embedding(src_seq)
-        tok_emb *= self.HIDDEN_DIM ** 0.5  ## normalization (root(d_model)을 곱함)
-        pos_emb = self.dropout(self.pos_embedding(tok_emb))
-        output = self.layer_norm(pos_emb)
+    def forward(self, src, src_mask):
 
-        for layer in self.layers:
-            output, attn_score = layer(output, src_mask)
-        return output
+        x = src
+        
+        x = self.tok_embedding(x)
+        x = x * self.scale
+        #src *= self.d_model**0.5
+        x = self.dropout(self.pos_encoding(x))
+        
+        
+        if self.attn_option == "CT":
+            topk_indices = None
+            output = x
+            for layer in self.layers:
+                output, topk_indices = layer(output, src_mask, topk_indices)
+            x = output
+        else:
+            for layer in self.layers :
+                x = layer(x, src_mask)     
 
-
+        return x        
         
